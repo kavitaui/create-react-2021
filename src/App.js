b20bcef82861103265606d05f6543a55bc7283c2
kavitaui <122557323+kavitaui@users.noreply.github.com>
2023-10-12 14:02:53 +0100

    create react basic app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react' {it is used bfore 17 version of react for jsx.}
 
-function App() {
+import React from "react"
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>welcome to kavita react learning. hello world.</h1>
+      <MyName />
+
     </div>
-  );
+  )
+}
+// how react is actually looking into it 
+// const App =()=>{
+//   return React.createElement(
+//     "div",
+//     {}
+//     react.createElement("h1",{},"Hello World")
+//   )
+// }
+// there are always use camel case in react attribute.
+//  how to create a nested componant.
+const MyName = () => {
+  return (<div><h1>Kavita Pandey</h1>
+ 
+  </div>)
 }
 
 export default App;
+
+
+
+
