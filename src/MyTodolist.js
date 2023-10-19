69c3200f6cbfa175f07475024667a409244a5437
kavitaui <122557323+kavitaui@users.noreply.github.com>
2023-10-19 10:54:12 +0100

    submit todo list

--- src/MyTodolist.js
+++ src/MyTodolist.js
@@ -21,37 +21,35 @@ const MyTodolist = () => {
         const changeData = list.filter(item => item.id !== id)
 
         setList(changeData);
-
-
+    }
+    const removeAll = () => {
+        setData([])
     }
     return (
         <div className='active'>
-            
-          < div className="my-todo">
-         <div className='child-div'>
-           
-            < input type="text" 
-                value={data}
-                onChange={(e) => { setData(e.target.value) }}
-                placeholder=" ✍️ input a new task"></input>
 
-            <button onClick={handleAddtodo}>Add</button>
-   
+            < div className="my-todo">
+                <div className='child-div'>
 
-            {list.map((item) => ( 
-                 
-                  <div className="text" key={item.id} >{item.todo}< input type="radio" className='my-text'
-                   onClick={() => { handleDeleteTodo(item.id) }} ></input>
-                </div>
-               
+                    < input type="text"
+                        value={data}
+                        onChange={(e) => { setData(e.target.value) }}
+                        placeholder=" ✍️ input a new task"></input>
 
-            )
-           
+                    <button onClick={handleAddtodo}>Add</button>
 
 
-            )}</div>
-             <button className='submit'>check list</button>
-         </div>
+                    {list.map((item) => (
+
+                        <div className="text" key={item.id} >{item.todo}< input type="radio" className='my-text'
+                            onClick={() => { handleDeleteTodo(item.id) }} ></input>
+                        </div>
+
+                    )
+              )
+                }</div>
+                <button className='submit' onClick={removeAll}>check list</button>
+            </div>
         </div>)
 
 }
